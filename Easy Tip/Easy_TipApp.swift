7bf82ae7776a48ec0dc42d906ae2d40fee67bda7
hannahtjacob <hannahtjacob@gmail.com>
2024-06-20 09:58:36 -0400

    Initial Commit

---
+++ Easy Tip/Easy_TipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_TipApp.swift
+//  Easy Tip
+//
+//  Created by Hannah Jacob on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Easy_TipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
